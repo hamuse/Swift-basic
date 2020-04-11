b9529bdef608e6c1fd03af2751532d0f9ef306be
한 여울 <53929403+hamuse@users.noreply.github.com>
2020-04-11 20:54:11 +0900

    swift study  update 20.04.11 pm: 08:53
    function 부분 공부

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -57,3 +57,99 @@ print(type(of: ii))
 var ii2 = 456 + ii
 print(ii2)
 
+
+
+// ---------------------------------------
+// function
+// ---------------------------------------
+
+//param X , return X
+func fn_1(){ //함수의 정의
+    print(">>>fn_1() 실행<<<")
+}
+
+//param O , return X
+func fn_2(a:Int, b:Int){
+    print("fn_2() 실행 a: \(a), b :\(b)")
+}
+
+//param X , return O
+func fn_3() -> Int {
+    print("fn_3() 실행")
+    return 100
+}
+
+//param O , return O
+func fn_4(a: Int , b: Int) -> Int {
+    print("fn_4() 실행 a: \(a) , b: \(b)")
+    return a + b
+}
+
+var r1 = fn_1() // 함수의 호출
+var r2 = fn_2(a: 10 , b: 20)
+var r3 = fn_3()
+var r4 = fn_4(a: 10, b : 20)
+var r4_2 = fn_4(a: 3, b: 4)
+
+print("r1:", r1)
+print("r2:", r2)
+print("r3:", r3)
+print("r4:", r4)
+print("r4_2:", r4_2)
+
+print("=====================")
+
+func fn_5(argA    paramA : Int, argB     paramB : String){
+        //인수이름  매개변수이름.     인수이름.  매개변수이름
+    print("fn_5() 실행 inA: \(paramA), inB: \(paramB)")
+    
+} //인수:  함수 호출시 사용
+  // 매개변수 : 함수 내부에서 사용
+
+func fn_6(_ paramA : Int , _ paramB : String){
+    print("fn_6() 실행 inA: \(paramA), inB: \(paramB)")
+} //인수 : _ 작성시 명시적 인수이름 작성 생략됨, 순서가 중요함
+//매개변수 : 함수 내부에서 사용
+
+fn_5(argA: 123, argB : "장동건")
+fn_6(789, "현빈")
+
+func exam(_ name: String, _ kor: Int, _ eng: Int, _ mat: Int){
+    let sum = kor + eng + mat
+    let avg = sum / 3
+    print("이름:" , name)
+    print("sum:" , sum)
+    print("avg:" , avg)
+}
+
+exam("한여울", 90, 100, 89)
+
+func exam2(_ name: String , _ jum:[Int]){
+    var sum = 0
+    for j in jum {
+       sum += j
+    }
+    let avg = sum / jum.count
+    
+    print("이름 : " , name)
+    print("sum : " , sum)
+    print("avg : " , avg)
+}
+
+exam2("한여울", [78,89,91,23])
+
+func exam3(_ name: String, _ jum: Int...){
+    var sum = 0
+    for j in jum {
+        sum += j
+    }
+    let avg = sum / jum.count
+    print("이름:", name)
+    print("총점:", sum)
+    print("평균:", avg)
+}
+
+exam3("hamuse", 100,60,70,80,80,70)
+
+
+
