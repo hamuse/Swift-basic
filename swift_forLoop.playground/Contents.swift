40ba09e04cada0392e0dc37ae7d1ea367aa346d6
한 여울 <53929403+hamuse@users.noreply.github.com>
2020-04-14 23:18:07 +0900

    swift study 20.04.14
    3버전 책으로 공부해서 에러가 많이 나는데 좀 찾아가면서 해야할거같다.

--- swift_forLoop.playground/Contents.swift
+++ swift_forLoop.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
 
 var str = "Hello, playground"
+
+for i in 1...10 {
+    print("Value of i is \(i)")
+}
+
+for _index in 1...5 {
+    print("Value of _index is \(_index)")
+}
+
+for char in "This is a string" {
+    print(char)
+}
+
+var myCount = 0
+
+while myCount < 100 {
+    myCount + 1
+    // ++myCount  -> swift 3 버전 책으로 공부해서 이부분이 에러가 남 4부터는 사용하지 않는건가... 바뀐건가.
+}
+
+
+
